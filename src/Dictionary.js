94adee93880ab9e1780174ad9ca28a3c128c846a
PatriciaKalinics <patriciakalinics@gmail.com>
2022-04-12 23:19:59 +0100

    added API call
    called via axios - imported

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,20 @@
+import axios from "axios";
 import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [query, setQuery] = useState("");
 
+  function handleResponse(response) {
+    let word = response.data[0];
+    console.log(word);
+    alert(`${word.word} (${word.meanings[0].partOfSpeech})`);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${query}...`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${query}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function updateQuery(event) {
