8bd4dbfb36bdcb3b6323e83486381736a5506317
PatriciaKalinics <patriciakalinics@gmail.com>
2022-04-12 21:37:20 +0100

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Welcome to the dictionary app!</header>
+      <footer className="App-footer">
+        <a
+          href="https://github.com/PatriciaKalinics/react-dictionary"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>{" "}
+        by Pat Kalinics
+      </footer>
+    </div>
+  );
+}
+
+export default App;
